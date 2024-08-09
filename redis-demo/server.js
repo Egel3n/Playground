9ed51b9771246c9a1c7c26f13aa31c953c740f52
Ege <egegelen@hotmail.com>
2024-08-09 14:14:57 +0300

    express session and redis merged

--- redis-demo/server.js
+++ redis-demo/server.js
@@ -2,6 +2,8 @@ import { client , create_session} from './services/redis-client.js';
 import { connection , checkUser} from "./services/mariadb-client.js"
 import {getProduct,getProductss} from './services/cacher.js'
 
+import RedisStore from "connect-redis"
+import session from "express-session"
 
 import express from "express";
 import bodyParser from "body-parser"
@@ -10,12 +12,31 @@ import cookieParser from 'cookie-parser';
 const app = express();
 const port = 3000;
 
+let redisStore = new RedisStore({
+  client: client,
+  prefix: "myapp:",
+})
+
+app.use(session({
+  store: redisStore,
+  secret: 'egelen',
+  resave: false,
+  saveUninitialized: true,
+}));
+
 app.use(bodyParser.json())
 app.use(cookieParser())
 
+
 app.get('/', async (req, res) => {
-  let products = await getProductss([1,21])
-  res.send(products);
+  console.log(req.session)
+  if (req.session.views) {
+    req.session.views++;
+} else {
+    req.session.views = 1;
+}
+res.send(`Views: ${req.session.views}`);
+  console.log(req.session)
 });
 
 app.post('/login', async (req,res)=>{
